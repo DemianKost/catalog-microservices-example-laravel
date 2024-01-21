893d34c063211e02f81c29a1395e63fbb1513148
DemianWHT <demiankost2@gmail.com>
2024-01-21 19:27:37 +0100

    Installed PEST for testing for client service

--- services/client/tests/Feature/Clients/ListClientsTest.php
+++ services/client/tests/Feature/Clients/ListClientsTest.php
@@ -2,6 +2,11 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\Clients\IndexController;
+use Illuminate\Auth\AuthenticationException;
+
 it('will throw an exception if not authenticated', function(): void {
-    
-});
+    $this->getJson(
+        uri: action(IndexController::class),
+    );
+})->throws(AuthenticationException::class);
