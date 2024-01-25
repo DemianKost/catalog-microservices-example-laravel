adff62177651344b090d0c397781ee8ed5778a7c
DemianWHT <demiankost2@gmail.com>
2024-01-25 19:54:29 +0100

    Added commands folder and CreateNewClient.php command and implemented it in the StoreController.php

--- services/client/app/Http/Controllers/Clients/StoreController.php
+++ services/client/app/Http/Controllers/Clients/StoreController.php
@@ -4,15 +4,34 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Clients;
 
+use App\Commands\Clients\CreateNewClient;
+use App\Exceptions\WriteException;
 use App\Http\Requests\Clients\StoreRequest;
+use Throwable;
 
 final class StoreController
 {
+    public function __construct(
+        private readonly CreateNewClient $command,
+    ) {}
+
     /**
      * @param StoreRequest $request
      */
     public function __invoke(StoreRequest $request)
     {
-        
+        $payload = $request->payload();
+
+        try {
+            $client = $this->command->handle(
+                payload: $payload,
+            );
+        } catch ( Throwable $exception ) {
+            throw new WriteException(
+                message: $exception->getMessage(),
+                code: $exception->getCode(),
+                previous: $exception
+            );
+        }
     }
 }
